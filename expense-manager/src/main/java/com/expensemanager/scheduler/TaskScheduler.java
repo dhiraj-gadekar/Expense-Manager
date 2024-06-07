f4c122a61eb8d6bd04fcce3425ac469461e9a620
dhiraj-gadekar <dhiraj.gadekar@nityaobject.com>
2024-06-07 11:14:22 +0530

    expenseReportGenerator() menthod call from TaskScheduler

--- expense-manager/src/main/java/com/expensemanager/scheduler/TaskScheduler.java
+++ expense-manager/src/main/java/com/expensemanager/scheduler/TaskScheduler.java
@@ -1,12 +1,18 @@
 package com.expensemanager.scheduler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import com.expensemanager.servise.MonthlyExpenseService;
+
 public class TaskScheduler {
+
+    @Autowired
+    private MonthlyExpenseService monthlyExpenseService;
     
     @Scheduled(cron = "0 0 0 1 * *")
     public void reportGeneratorScheduler() {
 
-        
+        monthlyExpenseService.expenseReportGenerator();
     }
 }
